546a51203b1f6834df9458a32358b206125c7978
aiyah-meloken <aiyah_meloken@protonmail.com>
2026-02-03 10:44:39 +0800

    fix: add pyo3-build-config to build.rs to correctly detect PyPy and resolving CI compilation errors

--- bindings/python/build.rs
+++ bindings/python/build.rs
@@ -1,4 +1,6 @@
 fn main() {
-    println!("cargo:rustc-check-cfg=cfg(Py_LIMITED_API)");
-    println!("cargo:rustc-check-cfg=cfg(Py_3_11)");
+    pyo3_build_config::use_pyo3_cfgs();
+
+    // Silence the unexpected_cfgs lint for PyPy since we are now properly setting it
+    println!("cargo:rustc-check-cfg=cfg(PyPy)");
 }
